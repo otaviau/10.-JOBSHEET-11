79c1c9109468e7231d13d037a1a2ab59db727ab9
uotavia <uotavia@gmail.com>
2025-05-31 01:01:20 +0700

    update numb 3 fix

--- Pertemuan12/SLLLMain20.java
+++ Pertemuan12/SLLLMain20.java
@@ -4,27 +4,65 @@ public class SLLLMain20 {
         Scanner sc = new Scanner(System.in);
         SingleLinkList20 sll = new SingleLinkList20();
 
-        System.out.print("Berapa banyak data yang ingin dimasukkan? ");
-        int jumlah = sc.nextInt();
-        sc.nextLine();
-
-        for (int i = 0; i < jumlah; i++) {
-            System.out.println("Data ke-" + (i+1));
-            System.out.print("NIM: ");
-            String nim = sc.nextLine();
-            System.out.print("Nama: ");
-            String nama = sc.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK: ");
-            double ipk = sc.nextDouble();
+        int pilihan;
+        do {
+            System.out.println("\n=== MENU LINKED LIST MAHASISWA ===");
+            System.out.println("1. Tambah data di awal");
+            System.out.println("2. Tambah data di akhir");
+            System.out.println("3. Sisipkan setelah nama tertentu");
+            System.out.println("4. Sisipkan di indeks tertentu");
+            System.out.println("5. Tampilkan semua data");
+            System.out.println("6. Keluar");
+            System.out.print("Pilih: ");
+            pilihan = sc.nextInt();
             sc.nextLine();
-            
-            Mahasiswa20 mhs = new Mahasiswa20(nim, nama, kelas, ipk);
-            sll.addLast(mhs);
-        }
 
-        System.out.println("=== Data Mahasiswa ===");
-        sll.print();
+            switch (pilihan) {
+                case 1:
+                    Mahasiswa20 mhsAwal = inputMahasiswa(sc);
+                    sll.addFirst(mhsAwal);
+                    break;
+                case 2:
+                    Mahasiswa20 mhsAkhir = inputMahasiswa(sc);
+                    sll.addLast(mhsAkhir);
+                    break;
+                case 3:
+                    System.out.print("Masukkan nama yang dicari: ");
+                    String namaKunci = sc.nextLine();
+                    Mahasiswa20 mhsAfter = inputMahasiswa(sc);
+                    sll.insertAfter(namaKunci, mhsAfter);
+                    break;
+                case 4:
+                    System.out.print("Masukkan index: ");
+                    int index = sc.nextInt();
+                    sc.nextLine();
+                    Mahasiswa20 mhsIndex = inputMahasiswa(sc);
+                    sll.insertAt(index, mhsIndex);
+                    break;
+                case 5:
+                    sll.print();
+                    break;
+                case 6:
+                    System.out.println("Terima kasih!");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        } while (pilihan != 6);
+    }
+
+    public static Mahasiswa20 inputMahasiswa(Scanner sc) {
+        System.out.print("NIM: ");
+        String nim = sc.nextLine();
+        System.out.print("Nama: ");
+        String nama = sc.nextLine();
+        System.out.print("Kelas: ");
+        String kelas = sc.nextLine();
+        System.out.print("IPK: ");
+        double ipk = sc.nextDouble();
+        sc.nextLine();
+        
+        return new Mahasiswa20(nim, nama, kelas, ipk);
     }
 }
+
